f593d175676ba7813d6037211e3c64111e584368
renanmarx26 <101154828+renanmarx26@users.noreply.github.com>
2024-05-15 18:34:08 -0300

    Aluno.java

---
+++ Aluno.java
@@ -0,0 +1,16 @@
+public class Aluno {
+
+    private String nome;
+    private  int idade;
+
+    public Aluno (String nome, int idade){
+        this.nome=nome;
+        this.idade=idade;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public int getIdade(){
+        return idade;
+    }
+}
